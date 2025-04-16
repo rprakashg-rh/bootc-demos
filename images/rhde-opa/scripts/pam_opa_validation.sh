43c0892cff245940caf678b73f99ca521304c1eb
Ram <rgopinat@redhat.com>
2025-04-15 21:52:47 -0700

    cleaned up pam hook script

--- images/rhde-opa/scripts/pam_opa_validation.sh
+++ images/rhde-opa/scripts/pam_opa_validation.sh
@@ -3,19 +3,18 @@
 # Exit as soon as any step fails with non-zero return code
 set -e 
 
-USER="$PAM_USER"
-
 #adding a back door
-if [[ $USER == 'admin']]; then
+if [[ "$PAM_USER" == "admin" ]]; then
+  echo "Admin"
   exit 0
 fi
 
 # Get all groups current user belongs to
-USER_GROUPS=$(id -nG "$USER" | tr ' ' ',')
+USER_GROUPS=$(id -nG "$PAM_USER" | tr ' ' ',')
 OPA_URL="http://localhost:8181/v1/data/access/allow"
 
 response=$(curl -X POST $OPA_URL -H "Content-Type: application/json" \
-  --data "{\"input\": {\"user\": \"$USER\", \"groups\": \"$USER_GROUPS\"}}")
+  --data "{\"input\": {\"user\": \"$PAM_USER\", \"groups\": \"$USER_GROUPS\"}}")
 
 allowed=$(echo "$response" | jq -r '.result')
 
